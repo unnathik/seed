a7b08773d7316954e6f41487592c425ecad51c62
Adhira Choudhury <adhirac@lawn-128-61-30-134.lawn.gatech.edu>
2024-02-11 00:01:10 -0500

    Changed navbar

--- seed/src/Navigation/AppNavbar.js
+++ seed/src/Navigation/AppNavbar.js
@@ -1,19 +1,93 @@
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Fragment, useState, useRef, useEffect } from 'react';
+import { Disclosure } from '@headlessui/react';
+import { Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 import seed from '../art_assets/seed_logo.png';
 import avatar from '../art_assets/avatar.png';
 import seed_text from '../art_assets/seed_text.png';
 
 const navigation = [
-  { name: 'My Portfolio', href: '/dashboard', current: true },
-  { name: '[PLACEHOLDER]', href: '/dashboard', current: false },
+  { name: 'My Portfolio', href: '/dashboard', current: true }
 ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
+const fetchStockInfo = async (stockSymbol) => {
+  if (stockSymbol.toUpperCase() === 'AAPL') {
+    return {
+      name: "Apple Inc.",
+      symbol: "AAPL",
+      chartData: [120, 122, 121, 123, 125],
+      info: "Apple Inc. is an American multinational technology company that specializes in consumer electronics, software, and online services."
+    };
+  }
+  return null;
+};
+
 export default function NavbarDefault() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showHoverBox, setShowHoverBox] = useState(false);
+  const [stockInfo, setStockInfo] = useState(null); // Initialize as null to properly handle conditional rendering
+  const searchInputRef = useRef(null);
+  
+  useEffect(() => {
+    const handleKeyDown = async (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault(); // Prevent the default form submit behavior
+        const stockData = await fetchStockInfo(searchTerm);
+        if (stockData) {
+          setStockInfo(stockData);
+          setShowHoverBox(true);
+        } else {
+          setShowHoverBox(false);
+        }
+      }
+    };
+    
+    const input = searchInputRef.current;
+    input.addEventListener('keydown', handleKeyDown);
+
+    return () => input.removeEventListener('keydown', handleKeyDown);
+  }, [searchTerm]);
+
+  const chartOptions = {
+    plugins: {
+      legend: {
+        display: false // Adjust based on your preference
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          display: false,
+        },
+        beginAtZero: false // Consider adjusting based on your data's expected range
+      },
+      y: {
+        ticks: {
+          display: false,
+        },
+        beginAtZero: true // Consider adjusting based on your data's expected range
+      }
+    },
+    maintainAspectRatio: false
+  };
+
+  const chartData = {
+    labels: stockInfo?.chartData?.map((_, index) => `Day ${index + 1}`) || [],
+    datasets: [
+      {
+        label: `${stockInfo?.name} Stock Price`,
+        data: stockInfo?.chartData || [],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }
+    ]
+  };
+
   return (
     <Disclosure as="nav" className="bg-[#f5f1e3] rounded-b-xl shadow-lg">
       {({ open }) => (
@@ -21,114 +95,43 @@ export default function NavbarDefault() {
           <div className="mx-auto w-full px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center py-5 w-full">
               <div className="flex items-center">
-                <img
-                  className="h-16 w-auto rounded-2xl"
-                  src={seed}
-                  alt="Seed"
-                />
-                <img
-                  className="h-8 w-auto pl-5"
-                  src={seed_text}
-                  alt="Seed"
-                />
+                <img className="h-16 w-auto rounded-2xl" src={seed} alt="Seed" />
+                <img className="h-8 w-auto pl-5" src={seed_text} alt="Seed" />
               </div>
-              
+              <div className="relative" ref={searchInputRef}>
+                    <input
+                      type="text"
+                      placeholder="Search..."
+                      className="bg-white text-black text-sm rounded-md focus:ring-black/[0.5] block w-96 p-2"
+                      value={searchTerm}
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    {showHoverBox && stockInfo && (
+                      <div className="absolute left-0 mt-2 w-96 bg-white rounded-md shadow-lg p-4 w-full">
+                        <h3 className="font-bold">{stockInfo.name} ({stockInfo.symbol})</h3>
+                        <p>{stockInfo.info}</p>
+                        <p>Recent Prices: {stockInfo.chartData.join(', ')}</p>
+                        {/* Chart rendering */}
+                        <div style={{ height: '300px' }}>
+                          <Line data={chartData} options={chartOptions} />
+                        </div>
+                      </div>
+                    )}
+              </div>
               <div className="flex items-center">
-                {/* Display navigation links side by side on large screens */}
-                <div className="hidden sm:block sm:mr-16">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'black' : 'text-gray-500 hover:text-gray-900',
-                          'px-3 py-2 rounded-md text-base font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a key={item.name} href={item.href} className={classNames(item.current ? 'text-black' : 'text-gray-500 hover:text-gray-900', 'px-6 py-2 rounded-md text-base font-medium')} aria-current={item.current ? 'page' : undefined}>
+                      {item.name}
+                    </a>
+                  ))}
                 </div>
-                
-                {/* Static Avatar for Larger Screens */}
-                <img
-                  className="hidden sm:block h-16 w-16 rounded-full"
-                  src={avatar}
-                  alt=""
-                />
-
-                {/* Clickable Avatar for Small Screens */}
-                <div className="sm:hidden">
-                  <Menu as="div">
-                    <div>
-                      <Menu.Button className="flex text-base rounded-full focus:outline-none">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-16 w-16 rounded-full"
-                          src={avatar}
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {navigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
+                <img className="hidden sm:block h-16 w-16 rounded-full" src={avatar} alt="" />
               </div>
             </div>
           </div>
-
-          {/* Mobile menu */}
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-700 hover:bg-gray-50 hover:text-gray-900',
-                    'block px-3 py-2 rounded-md text-md font-normal'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
   );
 }
-
-
